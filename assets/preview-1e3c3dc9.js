785ee0069960bdeec19105f83f795a92ce9285d6
TakanoriOnuma <TakanoriOnuma@users.noreply.github.com>
2024-02-03 09:41:25 +0000

    deploy: d2aac44c277b3f395b4b16b2bf604d42176e0072

--- assets/preview-1e3c3dc9.js
+++
@@ -1,2 +0,0 @@
-var a="myAddonParameter",e={globals:{[a]:{}}},r=e;export{r as default};
-//# sourceMappingURL=preview-1e3c3dc9.js.map
---
+++ assets/preview-3c73bdc3.js
@@ -0,0 +1,2 @@
+var a="myAddonParameterLegacy",e={globals:{[a]:{}}},r=e;export{r as default};
+//# sourceMappingURL=preview-3c73bdc3.js.map
